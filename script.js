f9dbf79fa4eba0e582a3209cbdde25d8dbb0b74f
Phillip Taylor <phillip.taylor.sns@googlemail.com>
2018-03-06 16:10:23 +0000

    Various style changes, added delete control on left pin, lock control on right pin.

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ $(function() {
 	ctx.fillStyle=bgPat;
 	ctx.fill();
 	*/
+	
+	var borderThick = 8;
+	
 	var lastId = 0;
 	var notes;
 	var lastHeight = 0;
@@ -18,11 +21,22 @@ $(function() {
 		notes = new Array();
 	else {
 		notes = JSON.parse(localStorage.getItem("notes"));
+		console.log(notes);
 		notes.forEach(function(currNote, ind,arr) {		
-			lastId++;
+			if(currNote.id > lastId) {
+				lastId = currNote.id;
+			}
 			if(currNote.z > lastHeight)
 				lastHeight = currNote.z;
-			noteHtml = "<textarea data-id = "+currNote.id+" class='note' style = 'width:"+currNote.width+"px;height:"+currNote.height+"px; position:absolute; top:"+currNote.y+"px;left:"+currNote.x+"px;z-index:"+currNote.z+"'>"+currNote.content+"</textarea>";
+			noteHtml = $("<div class = 'note-container' data-id = "+currNote.id+" style = 'position:absolute; top:"+currNote.y+"px;left:"+currNote.x+"px;z-index:"+currNote.z+"' ><textarea class='note-body' style='width:"+currNote.width+"px;height:"+currNote.height+"px;'>"+currNote.content+"</textarea></div>");
+			$(noteHtml).append("<img src = 'pin.png' data-id = "+currNote.id+"  class = \"control-pin\"/>");
+			if(currNote.pinned) {
+				$(noteHtml).append("<img src = 'pin-right.png' data-id = "+currNote.id+"  class = \"lock-pin\"/>");
+			}
+			else {
+				$(noteHtml).append("<img src = 'pin-right-hole.png' data-id = "+currNote.id+"  class = \"lock-pin-gone\"/>");
+			}
+			
 			console.log(currNote.content);
 			$("div#canvas").append(noteHtml);
 		});
@@ -33,7 +47,7 @@ $(function() {
 	var dragTarget;
 	var dragOffsetX = 0;
 	var dragOffsetY = 0;
-	var Note = function(x,y,h,w,content,z){		
+	var Note = function(x,y,h,w,content,z,pinned){		
 		this.id = ++lastId;
 		this.x = x;
 		this.y = y;
@@ -41,19 +55,104 @@ $(function() {
 		this.width = w;
 		this.content = content;
 		this.z = z;
+		this.pinned = pinned;
 	}
 	
-	$("div#canvas").on("click",".note", function(event) {
-		//console.log("?");
+	var SaveData = function() {
+		localStorage.setItem("notes",JSON.stringify(notes));
+	}
+	
+	var DeleteItem = function(dataId) {
+
+		var targNote = notes.findIndex(function(elem) {
+			return elem.id == dataId;
+		});
+		var target = $(".note-container").filter(function(elem) {			
+			return $(this).attr("data-id") == dataId;
+		});
+		if(notes[targNote].pinned == false) {
+			notes.splice(targNote,1);
+			$(target).remove();	
+		}
 		
+		
+		
+		SaveData();
+	}
+	$("div#canvas").on("mouseover",function(event) {
+		/*
+		console.log("{");
+		console.log("ScreenX",event.screenX,"ScreenY",event.screenY);
+		console.log("ClientX",event.clientX,"clientY",event.clientY);
+		console.log("PageX",event.pageX,"pageY",event.pageY);
+		console.log("OffsetX",event.offsetX,"OffsetY",event.offsetY);
+		console.log("ScrollLeft+ClientX",event.clientX+$("#canvas").scrollLeft(),"ScrollTop+ClientY",event.clientY+$("#canvas").scrollTop());
+		console.log("}");
+		*/
+		
+	});
+	$("div#canvas").on("click",".control-pin",function(event) {
+		DeleteItem($(this).attr("data-id"));
+		event.stopImmediatePropagation();
+	});
+	
+	$("div#canvas").on("mousedown",".control-pin",function(event) {
+		
+		event.stopImmediatePropagation();
+	});
+	
+	
+	$("div#canvas").on("click","img.lock-pin-gone",function(event) {
+		$(this).removeClass("lock-pin-gone").addClass("lock-pin");
+		$(this).attr("src","pin-right.png");
+		var tId = $(this).attr("data-id");
+		var targNote = notes.findIndex(function(elem) {
+			return elem.id == tId;
+		});
+		notes[targNote].pinned = true;
+		SaveData();
+		event.stopImmediatePropagation();
+	});
+	
+	$("div#canvas").on("click","img.lock-pin",function(event) {
+		$(this).removeClass("lock-pin").addClass("lock-pin-gone");
+		$(this).attr("src","pin-right-hole.png");
+		var tId = $(this).attr("data-id");
+		var targNote = notes.findIndex(function(elem) {
+			return elem.id == tId;
+		});
+		notes[targNote].pinned = false;
+		SaveData();
+		event.stopImmediatePropagation();
+	});
+	
+	$("div#canvas").on("click",".note-container", function(event) {
+		//console.log("?");
+		console.log("click-note-container");
 		event.stopImmediatePropagation();		
 	});
-	$("div#canvas").on("mousedown",".note", function(event) {
-		//console.log("?");
+	
+	$("div#canvas").on("mousedown",".note-body", function(event) {
+		console.log("mousedown-note-body");
+		event.stopImmediatePropagation();		
+	});
+	
+	$("div#canvas").on("mouseup",".note-body", function(event) {
+		console.log("MouseUp, note-body");
+		//event.stopImmediatePropagation();		
+	});
+	
+	$("div#canvas").on("mousedown",".note-container", function(event) {
+		//console.log("mousedown-note-container");
 		var tHeight = $(this).height();
 		var tTop = $(this).css("top").slice(0,-2);
+		var tId = $(this).attr("data-id");
+		var targNote = notes.findIndex(function(elem) {
+			return elem.id == tId;
+		});
+		console.log(notes[targNote]);
 		$(this).css("z-index",++lastHeight);
-		if(event.clientY < tHeight+(tTop-16)) {
+		if(event.clientY < tHeight+(tTop-20) && (notes[targNote].pinned == false)) {
 			isDraggingNote = 1;
 			dragTarget = this;
 		}
@@ -64,8 +163,8 @@ $(function() {
 		//console.log("?");
 		if(isDraggingNote == 1) {
 			isDraggingNote = 2;
-			dragOffsetX = event.offsetX;
-			dragOffsetY = event.offsetY;
+			dragOffsetX = event.offsetX+borderThick;
+			dragOffsetY = event.offsetY+borderThick;
 		}
 		if(isDraggingNote == 2) {
 			var tId = $(dragTarget).attr("data-id");
@@ -75,13 +174,13 @@ $(function() {
 			
 			
 			
-			var x = event.clientX-dragOffsetX;
+			var x = event.clientX-dragOffsetX + $("#canvas").scrollLeft();
 			//var x = event.clientX-100;
 			//var y = event.clientY-16;
-			var y = event.clientY-dragOffsetY;
-			console.log("1",event.clientX,dragOffsetX),
-			console.log("1",event.clientY,dragOffsetY),
-			console.log("3",x,y)
+			var y = event.clientY-dragOffsetY + $("#canvas").scrollTop();
+			//console.log("1",event.clientX,dragOffsetX),
+			//console.log("1",event.clientY,dragOffsetY),
+			//console.log("3",x,y)
 			$(dragTarget).css("top",y);
 			$(dragTarget).css("left",x);
 			
@@ -91,26 +190,31 @@ $(function() {
 		}
 		event.stopImmediatePropagation();		
 	});
-	$("div#canvas").on("mouseup",".note", function(event) {
-		//console.log("?");
-		isDraggingNote = 0;
-		var tId = $(dragTarget).attr("data-id");
-		var targNote = notes.findIndex(function(elem) {
-			return elem.id == tId;
-		});
-		notes[targNote].width = $(dragTarget).width();
-		notes[targNote].height = $(dragTarget).height();
-		notes[targNote].z = $(dragTarget).css("z-index");
-		localStorage.setItem("notes",JSON.stringify(notes));
-		event.stopImmediatePropagation();		
-	});
-	$("div#canvas").on('change keyup paste',".note", function(event) {
-		console.log("!!!")
+	$("div#canvas").on("mouseup",".note-container", function(event) {
+		console.log("Mouse-up, note container?");
+		if(isDraggingNote != 0) {
+			isDraggingNote = 0;
+		}
 		var tId = $(this).attr("data-id");
 		var targNote = notes.findIndex(function(elem) {
 			return elem.id == tId;
 		});
-		notes[targNote].content = $(this).val();
+		console.log("Note Container");
+		notes[targNote].width = $(this).width()-40;
+		notes[targNote].height = $(this).height();
+		notes[targNote].z = $(this).css("z-index");
+		localStorage.setItem("notes",JSON.stringify(notes));
+		
+		
+		event.stopImmediatePropagation();		
+	});
+	$("div#canvas").on('change keyup paste',".note-container", function(event) {
+		//console.log("!!!")
+		var tId = $(this).attr("data-id");
+		var targNote = notes.findIndex(function(elem) {
+			return elem.id == tId;
+		});
+		notes[targNote].content = $(this).children(".note-body").val();
 		localStorage.setItem("notes",JSON.stringify(notes));
 	});
 	
@@ -120,11 +224,11 @@ $(function() {
 		var h = 300;
 		var x = event.clientX - (w/2);
 		var y = event.clientY - 15;
-		var newNote = new Note(x,y,h,w,"",++lastHeight);
+		var newNote = new Note(x,y,h,w,"",++lastHeight,false);
 		//console.log(newNote);
 		
-		noteHtml = "<textarea data-id = "+newNote.id+" class='note' style = 'width:"+newNote.width+"px;height:"+newNote.height+"px; position:absolute; top:"+newNote.y+"px;left:"+newNote.x+"px;z-index="+newNote.z+"'></textarea>";
-		
+		noteHtml = $("<div data-id = "+newNote.id+" class = 'note-container' style = 'position:absolute; top:"+newNote.y+"px;left:"+newNote.x+"px;z-index="+newNote.z+"'><textarea class='note-body' style='width:"+newNote.width+"px;height:"+newNote.height+"px;'></textarea></div>");
+		$(noteHtml).append("<img src = 'pin.png' data-id = "+newNote.id+"  class = \"control-pin\"/>");
 		notes.push(newNote);
 		localStorage.setItem("notes",JSON.stringify(notes));
 		$("div#canvas").append(noteHtml);
